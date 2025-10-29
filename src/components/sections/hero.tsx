a563349b4aa467b1c55fc06c10f12fb107be511c
ManuLosta <mlostalom@gmail.com>
2025-10-29 17:20:48 -0300

    feat: add background effect

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button"
 import { ArrowRightIcon } from "lucide-react"
 import { motion } from "motion/react";
+import { LightRays } from "../ui/light-rays";
 
 export default function Hero({ onContact }: { onContact?: () => void }) {
   return (
@@ -12,6 +13,7 @@ export default function Hero({ onContact }: { onContact?: () => void }) {
         transition={{ duration: 0.6, ease: "easeOut" }}
         className="max-w-3xl mx-auto px-4 flex flex-col items-center justify-center gap-4"
       >
+        <LightRays />
         <h1 
         className="sm:text-6xl text-3xl text-primary text-center">
           <span className="font-bold">PRUEBA</span> DE CONCEPTO CON <span className="font-bold">RESULTADOS MEDIBLES</span> EN <span className="font-bold">30 DIAS</span>
@@ -19,7 +21,7 @@ export default function Hero({ onContact }: { onContact?: () => void }) {
         <p className="text-center text-muted-foreground text-lg">
           Sin compromisos ni venta forzada
         </p>
-        <Button size="lg" className="hover:scale-105 transition-all duration-300" onClick={onContact}>
+        <Button size="lg" className="hover:scale-105 transition-all duration-300 mt-4" onClick={onContact}>
           Contáctanos
           <ArrowRightIcon className="size-4" />
         </Button>
